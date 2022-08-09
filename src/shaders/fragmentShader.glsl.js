bf5b167fa87b1270e492d3c5aa97f4558134e9c1
Kacper Nadstoga <kapix007@gmail.com>
2022-08-09 20:17:46 +0200

    add basic toonShader

--- src/shaders/fragmentShader.glsl.js
+++ src/shaders/fragmentShader.glsl.js
@@ -1,9 +1,19 @@
 const fragmentShader = `
-    
-
-    void main(void) {
-      gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
+  varying vec3 lightdir;
+  varying vec3 eyenorm;
+  
+ void main() {
+    //vec3 lightdir = vec3 (1,1,2);
+    float ndotl = dot(normalize (eyenorm), normalize (lightdir));
+    if (ndotl > 0.8) {
+    ndotl = 1.0;
+    } else if (ndotl > 0.6) {
+    ndotl = 0.6;
+    } else {
+    ndotl = 0.2;
     }
+    gl_FragColor = vec4 (ndotl, ndotl, ndotl, 1.0);
+ }
 `;
 
 export default fragmentShader;
--- src/shaders/vertexShader.glsl.js
+++ src/shaders/vertexShader.glsl.js
@@ -1,7 +1,14 @@
 const vertexShader = `
+  varying vec3 lightdir;
+  varying vec3 eyenorm;
+  uniform vec3 lightpos;
+  void main() {
+    gl_Position = projectionMatrix* modelViewMatrix * vec4( position, 1.0);
 
-void main(void) {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vec4 tmp = modelViewMatrix * vec4 (lightpos, 1.0);
+    lightdir = tmp.xyz;
+
+    eyenorm = normalMatrix * normal;
   }
 `;
 
